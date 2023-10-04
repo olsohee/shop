b025e56d9ee7b810470d1938c6137e03284983ac
olsohee <dlthgml0108@daum.net>
2023-10-04 15:04:16 +0900

    delete: ProductImage 리포지토리 삭제

--- src/main/java/project/shop/repository/ProductImageRepository.java
+++
@@ -1,7 +0,0 @@
-package project.shop.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import project.shop.entity.product.ProductImage;
-
-public interface ProductImageRepository extends JpaRepository<ProductImage, Long> {
-}
